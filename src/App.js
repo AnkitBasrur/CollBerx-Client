341fc16b92cbf1caa819c90dc540a6a78f050c44
Ankit Basrur <49029704+AnkitBasrur@users.noreply.github.com>
2022-09-14 16:47:29 +0530

    desktop mode enabled

--- src/App.js
+++ src/App.js
@@ -12,30 +12,36 @@ import Demo from "./Demo";
 
 function App() {
   function getProps(data){
-    console.log(data)
+    
   }
-  return (
-    <ThemeContextProvider>
-          <Router>
-            <Switch>
-              <Route exact path="/">
-                <Login />
-              </Route>
-              <Route exact path="/home">
-                <Home getProps={getProps} />
-              </Route>
-              <Route exact path="/main/:id" component={Main} />
-              <Route exact path="/addRoom">
-                <AddRoom />
-              </Route>
-              <Route exact path="/demo">
-                <Demo />
-              </Route>
-              <Route exact path="/:id/logs" component={Logs} />
-            </Switch>
-          </Router>
-    </ThemeContextProvider>
-  );
+  const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+
+  if(isMobile)
+    return <div>Sorry, this website is available only on desktop devices.</div>
+  else{
+    return (
+      <ThemeContextProvider>
+            <Router>
+              <Switch>
+                <Route exact path="/">
+                  <Login />
+                </Route>
+                <Route exact path="/home">
+                  <Home getProps={getProps} />
+                </Route>
+                <Route exact path="/main/:id" component={Main} />
+                <Route exact path="/addRoom">
+                  <AddRoom />
+                </Route>
+                <Route exact path="/demo">
+                  <Demo />
+                </Route>
+                <Route exact path="/:id/logs" component={Logs} />
+              </Switch>
+            </Router>
+      </ThemeContextProvider>
+    );
+  }
 }
 
 export default App;
